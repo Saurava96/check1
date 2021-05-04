ae1754b0274ec067dbcf9ad4c060eb004cba0634
Saurav <internationalqueries@gmail.com>
2021-05-04 02:02:41 -0230

    Initial commit

--- src/main/java/com/example/demo/check2.java
+++ src/main/java/com/example/demo/check2.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +11,15 @@ public class check2 {
     @GetMapping(value = "/")
     public String Print(){
 
-        return "hekoo";
+        return "hello";
+
+
+    }
+
+    @PostMapping(value = "/posting")
+    public String PringPost(){
+
+        return "Posing";
 
     }
 
